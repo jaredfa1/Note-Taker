75a8c807be634a4f39b33b19bf1220e461046235
Jared Fanger <jared.fanger@gmail.com>
2020-09-08 17:25:09 -0600

    debugged

--- route/apiroute.js
+++ route/apiroute.js
@@ -1,5 +1,6 @@
 //Load
 var noteData = require("../db/db.json");
+const fs = require('fs');
 
 // Route
 module.exports = function (app){
@@ -8,10 +9,13 @@ module.exports = function (app){
     });
     console.log(noteData);
     app.post("api.notes", function (req, res){
-
+        noteData.push(req.body);
+        res.json(noteData);
+        
     });
     app.delete("/api/notes", function (req, res){
-
+        let delete = req.param.id;
+    
     });
 };
  
